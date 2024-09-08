46899a5128ba1b43c39b0511efe5d96701388fec
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2024-09-09 03:03:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar/Navbar"
+
+const App = () => {
+  return(
+    <main className="overflow-x-hidden">
+      <Navbar />
+    </main>
+  )
+}
+
+export default App;
